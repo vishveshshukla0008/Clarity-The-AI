50d1c707103f32e7bc5c02b9a95389ee39f5d7f0
unknown <vishveshshukla82@gmail.com>
2026-03-21 23:40:56 +0530

    server provided link chnaged

--- Frontend/src/api/httpClient.js
+++ Frontend/src/api/httpClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://clarity-the-ai.onrender.com/api",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
